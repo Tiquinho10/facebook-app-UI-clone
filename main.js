40fba40d1829e417b7344aa2bd042402821fc0ac
Luiz dos Santos Tique Júnior <38396043+Tiquinho10@users.noreply.github.com>
2021-02-12 13:51:43 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function like(){
+ // var gosto = document.getElementsByClassName('fa-thumbs-up')[0]
+  
+ //gosto.style.color = 'blue'
+  cor = document.getElementsByClassName('fa-thumbs-up')[0].style.color = "blue";
+ 
+}
